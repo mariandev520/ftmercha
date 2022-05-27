6eb5d4588e9924557a10019f64f0345098a59182
Mariano Manzo <marianomanzo@MacBook-Pro-de-Mariano.local>
2022-05-27 14:27:18 -0300

    beta13

--- components/FormService.js
+++ components/FormService.js
@@ -103,7 +103,7 @@ class FormService extends React.PureComponent {
         return (
 
             <section>
-            <form    onSubmit={ this.handleSubmit} >
+            <form class="animate__animated animate__backInUp  animate__delay-2s "   onSubmit={ this.handleSubmit} >
                 
      
 <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
