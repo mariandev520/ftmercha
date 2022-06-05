b774a733080cf6e20058f7a06cf1c5b0bc6b78c5
Mariano Manzo <marianomanzo@MacBook-Pro-de-Mariano.local>
2022-06-05 16:17:11 -0300

    beta23 Suma En Layout seccion

--- pages/euro.js
+++ pages/euro.js
@@ -2,63 +2,84 @@ import Layout from '../components/layout/Layout';
 import { euro } from '../cards'
 import Link from 'next/link';
 
-const Euro = () => {
+const euro = () => {
     return ( 
         <Layout>
-        <section className="body-font bg-gray-100">
-          <div className="py-24 mx-auto flex flex-wrap">
-            <div className="flex flex-wrap w-full bg-gray-100 md:py-24 px-10 relative mb-4">
-              <img alt="gallery" className="w-full object-cover md:h-auto  block absolute inset-0" src="img-section-main-euro-2.jpg"/>
-            </div>
+     <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Nuestras Acciones Promocionales Potencias tus Productos</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Nuestros equipos altamente capacitados con experiencia en cada zona pueden accionar y potenciar tus nuevos lanzamientos</p>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="lg:w-3/9 sm:w-2/6 p-8">
+        <div class="flex relative">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fmdq1-1.fna.fbcdn.net/v/t1.6435-9/86353382_2738775529509671_6069870455286661120_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEw_t8zbGgpogMC8f5LN57-ljJGv05tmBeWMka_Tm2YF6foiWz7VAKI6Bcz79dLLHg&_nc_ohc=Uli6kCWJznkAX8G57jw&_nc_ht=scontent.fmdq1-1.fna&oh=00_AT9lYh_iwJ50WpysVlac37hYkYVVLWFoyTprMGUOWoo75w&oe=62C3538D"></img>
+          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fmdq1-1.fna.fbcdn.net/v/t1.6435-9/50574224_2025268974193667_3296388696760123392_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGNe_-LtwAII7-ZxoDiLaBXmwh-IS3j3QibCH4hLePdCIx-jR2kRoqCKwIDJ4ybgkc&_nc_ohc=PrRpsrFSLnMAX9smvZV&_nc_ht=scontent.fmdq1-1.fna&oh=00_AT8ffa0GD72_ZA1_t8RCzZre0iQgYEildcnb-EkGm55DMA&oe=62C0D237"></img>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
+            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
           </div>
-        </section>
-        
-        
-        <section className="bg-gray-200 text-gray-600 body-font">
-          <div className="container md:mt-60 px-5 md:px-40 md:py-24 py-12 mx-auto">
-          <div className="flex flex-col text-center w-full mb-12">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-600">Acciones Promocionales</h1>
-            <div className="h-1 mx-auto bg-gradient-to-r from-yellow-500 to-yellow-600 w-24 mt-3 rounded mb-8"></div>
+        </div>
+      </div>
+      <div class="lg:w-3/9 sm:w-2/6 p-8">
+        <div class="flex flex-wrap relative">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fmdq1-1.fna.fbcdn.net/v/t1.6435-9/83288369_2690449584342266_6411430293596536832_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGPLOHD30sZdww_XupyZ4LSyvVaIxx860jK9VojHHzrSLvH049ergk0q1pd4oezG0c&_nc_ohc=pc5TryNoJb4AX9NoSCK&_nc_ht=scontent.fmdq1-1.fna&oh=00_AT-PjiLCdLYOYRwU6ETpW7zR_9vCjIeuzFfF9A7FjGLpcQ&oe=62C25B73"></img>
+          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fmdq1-2.fna.fbcdn.net/v/t1.6435-9/50060706_2025376264182938_3292689913874481152_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFCnDiDC4dsVmHSzBOjvmsWDlWLcwVneKAOVYtzBWd4oHqeS4e5VXmMhnd-AntWWAY&_nc_ohc=NvWOIR87mxQAX-rwE-3&_nc_ht=scontent.fmdq1-2.fna&oh=00_AT-ghsp_6-qalsLyW3haH8mqvaNuO_3jgIL_--RA4sIumA&oe=62C0C487"></img>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
+            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
           </div>
-          <div className="md:flex flex-wrap">          
-                      </div>
-                    </div>   
-                   
-                    <div class="grid grid-cols-2 gap-4">
-    <div class="bg-blue-300 w-52 h-72 m-8 static rounded-lg ">
-        <div class="bg-white w-52 h-72 -m-2 hover:m-0 absolute rounded-lg shadow-lg hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in ">
-            <h1 class="m-4 text-2xl font-bold">Wat is Lorem Ipsum?</h1>
-            <hr class="m-4 rounded-2xl border-t-2"></hr>
-            <p class="m-4 text-sm">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak</p>
         </div>
-    </div>
-    <div class="bg-green-300 w-52 h-72 m-8 static rounded-lg ">
-        <div class="bg-white w-52 h-72 hover:-m-2 absolute rounded-lg shadow-lg hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in ">
-            <h1 class="m-4 text-2xl font-bold">Wat is Lorem Ipsum?</h1>
-            <hr class="m-4 rounded-2xl border-t-2"></hr>
-            <p class="m-4 text-sm">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak</p>
+      </div>
+      <div class="lg:w-3/9 sm:w-2/6 p-8">
+        <div class="flex flex-wrap relative">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fmdq1-1.fna.fbcdn.net/v/t1.6435-9/83325443_2684247901629101_1170627593183952896_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGpCERD13gdFBKmminugdDfZpS2t-Y3Fb1mlLa35jcVvUnQCjUAQv9ftaA-mYYHGlo&_nc_ohc=B2z8ypTVP7wAX-WRkO2&_nc_ht=scontent.fmdq1-1.fna&oh=00_AT_KE7d6QTXSN00F0vvV-vuhU01GxFOIdjL7kFnevSGjjA&oe=62C36102"></img>
+          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fmdq1-1.fna.fbcdn.net/v/t1.6435-9/59659871_2182238625163367_4376646488659329024_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFNe5nw_8NxImaEA6EmnxdsfzsHzCghEtt_OwfMKCES2_h6DV0uPJzVfihX9neWvA8&_nc_ohc=xHheIy8Q4EQAX-7VkO0&tn=B8mkDRDuUPE7Gzmd&_nc_ht=scontent.fmdq1-1.fna&oh=00_AT_MKFe8X8Ai6W5smzwwXqZO-x72qYvunsZXfdKRJ9AnfQ&oe=62C375F5"></img>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
+            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+          </div>
         </div>
-    </div>
-    <div class="bg-red-300 w-52 h-72 m-8 static rounded-lg ">
-        <div class="bg-white w-52 h-72 hover:m-2 absolute rounded-lg shadow-lg hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in ">
-            <h1 class="m-4 text-2xl font-bold">Wat is Lorem Ipsum?</h1>
-            <hr class="m-4 rounded-2xl border-t-2"></hr>
-            <p class="m-4 text-sm">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak</p>
+      </div>
+      <div class="lg:w-3/9 sm:w-2/6 p-8">
+        <div class="flex flex-wrap relative">
+        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fmdq1-1.fna.fbcdn.net/v/t1.6435-9/59659871_2182238625163367_4376646488659329024_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFNe5nw_8NxImaEA6EmnxdsfzsHzCghEtt_OwfMKCES2_h6DV0uPJzVfihX9neWvA8&_nc_ohc=xHheIy8Q4EQAX-7VkO0&tn=B8mkDRDuUPE7Gzmd&_nc_ht=scontent.fmdq1-1.fna&oh=00_AT_MKFe8X8Ai6W5smzwwXqZO-x72qYvunsZXfdKRJ9AnfQ&oe=62C375F5"></img>
+          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fmdq1-1.fna.fbcdn.net/v/t1.6435-9/59659871_2182238625163367_4376646488659329024_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFNe5nw_8NxImaEA6EmnxdsfzsHzCghEtt_OwfMKCES2_h6DV0uPJzVfihX9neWvA8&_nc_ohc=xHheIy8Q4EQAX-7VkO0&tn=B8mkDRDuUPE7Gzmd&_nc_ht=scontent.fmdq1-1.fna&oh=00_AT_MKFe8X8Ai6W5smzwwXqZO-x72qYvunsZXfdKRJ9AnfQ&oe=62C375F5"></img>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Neptune</h1>
+            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+          </div>
         </div>
-    </div>
-    <div class="bg-yellow-300 w-52 h-72 m-8 static rounded-lg ">
-        <div class="bg-white w-52 h-72 m-2 hover:m-0 absolute rounded-lg shadow-lg hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in ">
-            <h1 class="m-4 text-2xl font-bold">Wat is Lorem Ipsum?</h1>
-            <hr class="m-4 rounded-2xl border-t-2"></hr>
-            <p class="m-4 text-sm">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak</p>
-        </div>
-    </div>
+      </div>
+      <div class="lg:w-3/9 sm:w-2/6 p-8">
 
-</div>
-          </section>
+        <div class="flex flex-wrap relative">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fmdq1-2.fna.fbcdn.net/v/t1.6435-9/59789738_2182248768495686_2817037249508540416_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGWIrIJpH7SS972UU0cc-xQuMW1v4gMJyy4xbW_iAwnLI2xugmUzfiCNHd-xmqbQx8&_nc_ohc=uig_1z4NIrMAX8pdh9P&_nc_ht=scontent.fmdq1-2.fna&oh=00_AT8s-Mcv5twzprEPSqGwhc8OvD5f55gjWjwC6bzg7ei-qQ&oe=62C3E2C1"></img>
+          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fmdq1-1.fna.fbcdn.net/v/t1.6435-9/36364106_1735449016508999_6952633075251544064_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeECD_IadZ-Z5KOA0EgweIUonZjUGB0vT4ydmNQYHS9PjLMiefZs5Zsv1lre6Aq4cRw&_nc_ohc=Z7zpasxJ5oIAX8Z8119&_nc_ht=scontent.fmdq1-1.fna&oh=00_AT_ZpDAo79KwZ8eZgvZ1kboJe_ISWsoGIZ9EHJ7BrBjLQw&oe=62C0D20B"></img>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Holden Caulfield</h1>
+            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+          </div>
+        </div>
+      </div>
+      <div class="lg:w-3/9 sm:w-2/6 p-8">
+
+        <div class="flex flex-wrap relative">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fmdq1-2.fna.fbcdn.net/v/t1.6435-9/50155058_2025278224192742_3178733758517870592_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFhNfQeusWZLjWT_2L4NDt1nixhLmOxZyCeLGEuY7FnIJRvMZVVYefp63YZfnsKrQc&_nc_ohc=Hko4NZ9Z1T8AX_r7gLZ&_nc_ht=scontent.fmdq1-2.fna&oh=00_AT_8GmmX_dFCjTA-3MOodNChhldpzFoBUI88HZ-9gMXUEw&oe=62C0A9D2"></img>
+          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://scontent.fmdq1-1.fna.fbcdn.net/v/t1.6435-9/44786364_1904715472915685_4846744781058998272_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGHNMwOI7GlTJLPBBaX579gPHhGZXLyUYY8eEZlcvJRhtC1lY3n9o0uNAkSzDNY47A&_nc_ohc=HMiptPhCz6gAX9xSPxO&tn=B8mkDRDuUPE7Gzmd&_nc_ht=scontent.fmdq1-1.fna&oh=00_AT-0KK4JRYS_INf5K_jNGxm6Gj9IdknU1l-eHChkZM_0fQ&oe=62C231BE"></img>
+            <h2 class="title-font text-lg font-medium text-gray-900 mb-3">Alper Kamu</h2>
+            <h2 class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
           
         </Layout>
      );
 }
  
-export default Euro;
+export default euro;
