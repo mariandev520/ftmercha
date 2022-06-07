4e56aea22c3f21e9dcfc487cc02912427afd6d0a
Mariano Manzo <marianomanzo@MacBook-Pro-de-Mariano.local>
2022-06-07 10:23:11 -0300

    beta26

--- pages/patagonia.js
+++ pages/patagonia.js
@@ -16,55 +16,36 @@ const Patagonia = () => {
         
         
         <div className="py-24 mx-auto flex flex-wrap">
-          <div className="flex flex-wrap w-full bg-gray-100 md:py-24 px-10 relative mb-4">
-      
-<div className='flex items-center justify-center min-h-screen from-teal-100 via-teal-300 to-teal-500 bg-gradient-to-br'>
-  <div
-            className="overflow-hidden  aspect-video bg-red-400 cursor-pointer rounded-xl relative group"
-        >
-            <div
-                className="rounded-xl z-50 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out cursor-pointer absolute from-black/80 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end"
-            >
-                <div>
-                    <div
-                        className="transform-gpu  p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-10 transform transition duration-300 ease-in-out"
-                    >
-                        <div className="font-bold">Jessie Watsica</div>
-
-                        <div className="opacity-60 text-sm ">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Distinctio dolores error iure, perferendis
-                            sequi totam. Ad aliquam aperiam atque deleniti dolor
-                            dolorem enim esse et in, inventore itaque, pariatur
-                            reprehenderit.
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <img
-                alt=""
-                class="object-cover w-full aspect-square group-hover:scale-110 transition duration-300 ease-in-out"
-                src="img-section-main-patagonia-2.jpg"
-            />
-        </div>
- </div>
-           
-          </div>
-        </div>
-      </section>
-
-    
-     
-
-      <section className="text-gray-600 body-font">
-     
-          <div class="h-screen flex items-center justify-center">
-  
- 
+       
 </div>
+      
+    
+ 
+      <section class="text-gray-600 body-font relative">
+  <div class="absolute inset-0 bg-gray-300">
+    <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d16370307.098038433!2d-69.02740539823158!3d-33.79328432269329!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccaf5f5fdc667%3A0x3d2f77992af00fa8!2sArgentina!5e0!3m2!1ses!2sar!4v1654607907252!5m2!1ses!2sar" ></iframe>
+  </div>
+  <div class="container px-5 py-24 mx-auto flex">
+    <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Consulta Tus zonas deseadas</h2>
+      <p class="leading-relaxed mb-5 text-gray-600">Nuestra Cobertura a nivel Nacional servicio exclusivo de Nuestros Clientes</p>
+      <div class="relative mb-4">
+        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+      </div>
+      <div class="relative mb-4">
+        <label for="message" class="leading-7 text-sm text-gray-600">Consulta Tu zona Deseada</label>
+        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+      </div>
+      <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Enviar</button>
+      <p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
+    </div>
+  </div>
 </section>
-  
+
+    </section>
      
+
 
 
     </Layout>
