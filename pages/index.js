9afdc991003b23be5fbab8ff73ba25c30f0423d5
Mariano Manzo <marianomanzo@MacBook-Pro-de-Mariano.local>
2022-06-09 09:48:49 -0300

    beta29

--- pages/index.js
+++ pages/index.js
@@ -101,10 +101,10 @@ const Index = () => {
           <div class="items-center flex flex-wrap">
             <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
               <div class="pr-12">
-                <h1 class="text-white font-semibold text-5xl">
-                 Su Crecimiento esta asegurado
+                <h1 class=" font-semibold text-5xl text-gray-400 hover:text-green-800">
+                 Crecimiento Garantizado 
                 </h1>
-                <p class="mt-4 text-lg text-blueGray-200">
+                <p class="mt-4 text-lg text-gray-700">
                   Nuestros equipos estan al servicio de sus productos 
                 </p>
               </div>
@@ -127,9 +127,8 @@ const Index = () => {
                     <i class="fas fa-award"></i>
                   </div>
                   <h6 class="text-xl text-gray-600  font-semibold">Agencia Laboral</h6>
-                  <p class="mt-2 mb-4 text-blueGray-500">
-                    Divide details about your product or agency work into parts.
-                    A paragraph describing a feature will be enough.
+                  <p class="mt-2 mb-4 text-green-700">
+                   Años de experiencia aseguran nuestro organizacion de los recursos humanos mas espeficicos y dedicados.
                   </p>
                 </div>
               </div>
@@ -142,8 +141,7 @@ const Index = () => {
                   </div>
                   <h6 class="text-xl text-gray-600 font-semibold">Inversiones Seguras</h6>
                   <p class="mt-2 mb-4 text-green-700">
-                    Keep you user engaged by providing meaningful information.
-                    Remember that by this time, the user is curious.
+                    Su inversion en nuestros servicios potencia su negocio a lo largo del desarrollo de su cartera de productos y clientes.
                   </p>
                 </div>
               </div>
@@ -154,10 +152,9 @@ const Index = () => {
                   <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
                     <i class="fas fa-fingerprint"></i>
                   </div>
-                  <h6  class="text-xl  text-gray-600  font-semibold">Marketing Digital</h6>
-                  <p class="mt-2 mb-4 text-blueGray-500">
-                    Write a few lines about each one. A paragraph describing a
-                    feature will be enough. Keep you user engaged!
+                  <h6  class="text-xl  text-gray-600  font-semibold">Marketing Grafica Digital</h6>
+                  <p class="mt-2 mb-4 text-green-700">
+                  Publicidad gráfica especializada en carteleria, señalización, corporeos, leds, exhibidores , fotografía, decoración de vehículos, puntos de venta.
                   </p>
                 </div>
               </div>
@@ -169,7 +166,7 @@ const Index = () => {
     <div class="flex flex-wrap items-center md:justify-between justify-center">
       <div class="w-full md:w-6/12 px-4 mx-auto text-center">
         <div class="text-sm text-blueGray-500 font-semibold py-1">
-          Made with <a href="https://www.creative-tim.com/product/notus-js" class="text-blueGray-500 hover:text-gray-800" target="_blank">Notus JS</a> by <a href="https://www.creative-tim.com" class="text-blueGray-500 hover:text-blueGray-800" target="_blank"> Creative Tim</a>.
+        Desing <a class="text-blueGray-500 hover:text-gray-800" target="_blank"></a> by <a href="https://www.mad.net.ar" class="text-blueGray-500 hover:text-gray-800" target="_blank"> MAD</a>.
         </div>
       </div>
     </div>
