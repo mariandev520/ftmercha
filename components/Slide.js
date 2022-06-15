2420d5d37848718c16f1f93483cd8bea3b897158
Mariano Manzo <marianomanzo@MBP-de-Mariano.fibertel.com.ar>
2022-06-15 09:09:23 -0300

    beta34

--- components/Slide.js
+++ components/Slide.js
@@ -34,7 +34,7 @@ const Slide = () => {
             onSwiper={(swiper) => console.log(swiper)}
         >
             <SwiperSlide transition-style="in:square:bottom-right">  <img  onClick={dhat} src='move22.svg' className="md:h-screen w-screen object-cover md:mt-26"/></SwiperSlide> 
-            <SwiperSlide > <img  onClick={dhat} src='movi1.svg'  className="md:h-screen w-screen object-cover md:mt-26"/> </SwiperSlide>
+            <SwiperSlide > <img  onClick={dhat} src='meta1.png'  className="md:h-screen w-screen object-cover md:mt-26"/> </SwiperSlide>
             <SwiperSlide><img  onClick={dhat} src="/test6 .gif" className="md:h-screen w-screen object-cover md:mt-26"/></SwiperSlide>
      
             <SwiperSlide><img onClick={dhat} src="/tema5.png" className="md:h-screen w-screen object-cover md:mt-26"/></SwiperSlide>
