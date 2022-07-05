92c4bf76216721708bec9a753582a6de4acea18a
Mariano Manzo <marianomanzo@MBP-de-Mariano.fibertel.com.ar>
2022-07-05 10:37:48 -0300

    beta...3

--- pages/index.js
+++ pages/index.js
@@ -190,6 +190,27 @@ const Index = () => {
   </div>
 </section>
       </section>
+
+      
+<div class="bg-white h-screen flex items-center justify-center p-12 py-6">
+  <div class="mx-auto w-full max-w-screen-lg bg-green-400 px-5 py-10">
+    <div class="grid gap-5 md:grid-cols-2 md:gap-10 lg:gap-20">
+      <div class="flex justify-center md:justify-end">
+        <img class="w-full max-w-sm" src="https://ouch-cdn2.icons8.com/sKnF2PmYhkmP28DzIm6KqWSknT03UVWjg3FLlGYIOp4/rs:fit:684:456/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvOTI3/L2U4OWQ2NmZiLTg0/NzEtNDc3NS1hNTA0/LTMwNWRiYmJkNzg0/MC5zdmc.png" alt="Marketing newsletter via computer Illustration in PNG, SVG" />
+      </div>
+      <div class="flex items-center">
+        <div class="mx-auto md:mx-0">
+          <h3 class="text-4xl font-bold text-white">Suscribirse</h3>
+          <p class="mt-2 max-w-[20rem] text-lg text-white/80">Recibe las últimas novedades del sector  </p>
+          <form action="" class="mt-4 flex flex-col">
+            <input type="email" name="email" id="email" placeholder="Ingresa Tu Email" class="w-full rounded border border-white/50 bg-transparent px-3 py-2 text-white placeholder:text-white/50 md:max-w-[18rem]" />
+            <button type="submit" class="mt-4 w-full max-w-[14rem] rounded bg-white/30 px-14 py-2 text-center text-white">Subscribe</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
  
       </Layout>
     </div>
